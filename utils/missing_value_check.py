c1576cccfccd3a1b4867de64dc0ca806f68a1999
salhit <tarek.salhi@vodafone.com>
2019-08-23 11:58:36 +0100

    Adding missing value helper function

---
+++ utils/missing_value_check.py
@@ -0,0 +1,17 @@
+def missing_values_table(df):
+    """
+    Output a report detailing the total number of features containing missing values, together with the
+    percentage missing. 
+    """
+    mis_val = df.isnull().sum()
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+    mis_val_table_ren_columns = mis_val_table.rename(
+    columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+    '% of Total Values', ascending=False).round(1)
+    print ("The dataframe has " + str(df.shape[1]) + " columns.\n"      
+        "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+    return mis_val_table_ren_columns
